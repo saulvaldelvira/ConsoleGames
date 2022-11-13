2bf6c0227180837cc7742e75146043f9befcef68
Saúl <saulvaldelvira@gmail.com>
2022-11-13 14:29:21 +0100

    Make

--- build.sh
+++ build.sh
@@ -1,11 +1,39 @@
-echo Building Suite ...
+action=UNKNOWN
+unknown=UNKNOWN
+while [ "$action" = "$unknown" ];
+do
+    echo "***Options***"
+    echo "1)Build Release"
+    echo "2)Build Debug"
+    echo "3)Build All"
+    echo "4)Clean"
 
-cd Guessing
-make release
-cd ..
+    read -p "Option: " option
+    echo "$option"
+    
 
-cd Snake
-make release
-cd ..
+    if [ "$option" = "1" ];
+    then
+        action="release"
+    elif [ "$option" = "2" ];
+    then
+        action="debug"
+    elif [ $option = "3" ];
+    then
+        action="all"
+    elif [ $option = "4" ];
+    then
+        action="clean"
+    else
+        echo "Invalid Option"
+    fi
+done
 
-echo Finished
+for dir in ./src/*/
+do
+    echo working on $dir
+    cd $dir
+    make $action
+    cd ../..
+    echo
+done
